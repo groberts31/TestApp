ca6f1498ae493f3edf7abd0e28eac5b58d924f43
Greg Roberts <groberts3104@gmail.com>
2020-11-05 21:04:27 -0500

    installed react-router-dom for Router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./family.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, My Name is Greg!!!</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, My Name is Greg!!!</h1>
+        </header>
+        <p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </p>
+      </div>
+    </Router>
   );
 }
 
